8278fc8ca5b2beabf4ccb71e4949997867785384
Nguyễn Văn Tâm <tam.nv.61cntt@ntu.edu.vn>
2024-03-26 12:40:53 +0700

    Rename folder

--- BaiThi/Cau2_AppBMI(Java_Android)/app/src/main/java/ntu_61132086/cau1_appbmi/DislayResult.java
+++ BaiThi/Cau2_AppBMI(Java_Android)/app/src/main/java/ntu_61132086/cau1_appbmi/DislayResult.java
@@ -15,10 +15,6 @@ public class DislayResult extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dislay_result);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
